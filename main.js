5ad9e772135d3da3e0c416db3557f2d068df8f83
vimaljain0611 <vimal.webosmotic@gmail.com>
2021-07-05 15:44:42 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+class Main {
+    constructor(formId,storageId)
+    {
+        this.formId = formId;
+        this.storageId = storageId;
+    }
+    
+
+}
+const form = new Form (formId);
+form.onRemove();
+
+const form = new Storage (storageId);
+{
+    
+}
+
+const create = new Main("dynamicForm","formData");
