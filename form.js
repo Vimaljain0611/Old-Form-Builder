f4e1c336a372dc577971b04e2a5a99c3bbfc7fcf
vimaljain0611 <vimal.webosmotic@gmail.com>
2021-07-05 19:22:24 +0530

    Enter

--- form.js
+++ form.js
@@ -1,7 +1,114 @@
+var dropDownOptions = [  
+{innerHTML : 'Text' , value : 'text'},
+{innerHTML : 'Number' , value : 'number'},
+{innerHTML : 'Date' , value : 'date'},
+{innerHTML : 'Range' , value : 'range'},
+{innerHTML : 'Email' , value : 'email'},
+{innerHTML : 'Color' , value : 'color'}
+];
 class Form{
+    constructor(formId){
+        this.formId = formId;
+    }
 
-    createElement()
+    //create Table
+    createTable()
     {
-        
+        let table = document.createElement('table');
+            table.className = "table table-responsive-sm";
+            table.id= this.formId;
+           
+        return table;            
+    }
+
+    //create thead
+    createThead()
+    {
+            let thead= document.createElement('thead');
+            let row = document.createElement('tr');
+                row.name ="trRow";
+                row.className = "form-row";
+                
+                let labelTh = document.createElement('th');
+                let label = this.createEle('input','text');
+                    label.className="dyn-label"
+                    label.placeholder = "Enter Label / Id";
+                labelTh.appendChild(label);    
+                row.appendChild(labelTh);
+                
+                let dropDownTh = document.createElement('th');
+                let dropDown = document.createElement('select');
+                    dropDown.className="input-id";
+                    dropDownOptions.forEach((opt, index) =>{
+                        let optNode = document.createElement("option");
+                            optNode.value = opt.value;
+                            optNode.innerHTML = opt.innerHTML;
+                        return dropDown.appendChild(optNode);
+                    });
+                dropDownTh.appendChild(dropDown);
+                row.appendChild(dropDownTh);
+
+                let addBtnTd = document.createElement('td');   
+                let addFunction= function()  { console.log(row.childNodes ); }   
+                addBtnTd.appendChild(this.createButton("Add",addFunction)); 
+                row.appendChild(addBtnTd);
+            
+                let refreshBtnTd = document.createElement('td'); 
+                let refreshFunction= function() {  console.log(this);}   
+                refreshBtnTd.appendChild(this.createButton("Refresh",refreshFunction)); 
+                row.appendChild(refreshBtnTd);
+
+            thead.appendChild(row);      
+            return thead;       
+    }
+    // create tbody
+    createTbody()
+    {
+        let tbody = document.createElement('tbody');
+        return tbody;            
+    }
+
+    //creating Tr
+    createTr(label,type)
+    {
+        let row = document.createElement('tr'); 
+            
+            let labelTd = document.createElement('td');
+                labelTd.appendChild(this.createEle('label','',label));
+            row.appendChild(labelTd);
+
+            let inputTd = document.createElement('td');    
+                inputTd.appendChild(this.createEle('input',type));
+            row.appendChild(inputTd);
+
+            let saveBtnTd = document.createElement('td');   
+                let saveFunction= function() { console.log(this.label);}    
+                saveBtnTd.appendChild(this.createButton("Save",saveFunction)); 
+            row.appendChild(saveBtnTd);
+            
+            let removeBtnTd = document.createElement('td'); 
+                let removeFunction= function() { console.log(this.label);}   
+            removeBtnTd.appendChild(this.createButton("Remove",removeFunction)); 
+            row.appendChild(removeBtnTd);
+
+        return row; 
+    }
+
+    //create elements
+    createEle(Elem,type,text)
+    {
+        let input = document.createElement(Elem);
+            input.type = type;
+            input.textContent = text;
+        return input;
+    }
+
+    //create buttons
+    createButton(label,btnFunction)
+    {
+        let btn = document.createElement('button');
+            btn.textContent =label;
+            btn.onclick= btnFunction; 
+        return btn;
     }
 }
--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-class Main {
+class Main{
+    
     constructor(formId,storageId)
     {
         this.formId = formId;
         this.storageId = storageId;
+        
+        //classes
+        this.form = new Form (this.formId);
+        this.storage = new Storage(this.storageId);
+
+        //creating Table
+        this.table = this.formView.appendChild(this.form.createTable());
+
+        //appending Thead
+        this.table.appendChild(this.form.createThead());
+
+        // appending Tbody
+        this.tbody = this.table.appendChild(this.form.createTbody());
+
     }
-    
+    //getting div id
+    formView = document.getElementById('createForm');
+   
+    appendInTbody(Label,Type){
+        this.tbody.appendChild(this.form.createTr(Label,Type));
+    }
 
-}
-const form = new Form (formId);
-form.onRemove();
-
-const form = new Storage (storageId);
-{
-    
 }
 
 const create = new Main("dynamicForm","formData");
+
+create.appendInTbody('id1','text');
+create.appendInTbody('id2','color');
