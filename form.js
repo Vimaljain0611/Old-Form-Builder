2f11b057f53625c140e92132500464306e0239a8
vimaljain0611 <vimal.webosmotic@gmail.com>
2021-07-06 19:34:42 +0530

    Design changed and added add function on button

--- form.js
+++ form.js
@@ -1,114 +1,130 @@
-var dropDownOptions = [  
-{innerHTML : 'Text' , value : 'text'},
-{innerHTML : 'Number' , value : 'number'},
-{innerHTML : 'Date' , value : 'date'},
-{innerHTML : 'Range' , value : 'range'},
-{innerHTML : 'Email' , value : 'email'},
-{innerHTML : 'Color' , value : 'color'}
-];
-class Form{
-    constructor(formId){
-        this.formId = formId;
+class Form {
+  setDropDownOptions;
+  formId;
+  //mainClass;
+  constructor(formId) {
+    this.declaration(formId);
+  }
+
+  declaration(formId) {
+    this.formId = formId;
+
+    this.setDropDownOptions = [
+      { innerHTML: "Text", value: "text" },
+      { innerHTML: "Number", value: "number" },
+      { innerHTML: "Date", value: "date" },
+      { innerHTML: "Range", value: "range" },
+      { innerHTML: "Email", value: "email" },
+      { innerHTML: "Color", value: "color" },
+    ];
+  }
+
+  //create thead
+  getFormRow(mainClass) {
+    const form = document.createElement("form");
+    form.id = this.formId;
+    const row = document.createElement("div");
+    row.className = "row";
+
+    const labelCol = document.createElement("div");
+    labelCol.className = "col-md-3";
+    labelCol.appendChild(this.createEle("input", "text"));
+    row.appendChild(labelCol);
+
+    let selectCol = document.createElement("div");
+    selectCol.className = "col-md-3";
+
+    const dropDown = document.createElement("select");
+    dropDown.className = "form-control";
+    this.setDropDownOptions.forEach((opt, index) => {
+      const optNode = document.createElement("option");
+      optNode.value = opt.value;
+      optNode.innerHTML = opt.innerHTML;
+      return dropDown.appendChild(optNode);
+    });
+
+    selectCol.appendChild(dropDown);
+    row.appendChild(selectCol);
+
+    let addBtnCol = document.createElement("div");
+    addBtnCol.className = "col-md-3";
+    const addFunction = function (e) {
+      e.preventDefault();
+      mainClass.addRow(this.form);
+    };
+    addBtnCol.appendChild(this.createButton("Add", addFunction));
+    row.appendChild(addBtnCol);
+
+    let refreshBtnCol = document.createElement("div");
+    refreshBtnCol.className = "col-md-3";
+    const refreshFunction = function () {
+      console.log(this);
+    };
+    refreshBtnCol.appendChild(this.createButton("Refresh", refreshFunction));
+    row.appendChild(refreshBtnCol);
+
+    form.appendChild(row);
+    return form;
+  }
+
+  //creating 
+  createRow(label, type, mainClass) {
+    const row = document.createElement("div");
+    row.className = "row";
+
+    const labelCol = document.createElement("div");
+    labelCol.className = "col-md-3";
+    labelCol.appendChild(this.createEle("label", "", label));
+    row.appendChild(labelCol);
+
+    const inputCol = document.createElement("div");
+    inputCol.className = "col-md-3";
+    inputCol.appendChild(this.createEle("input", type));
+    row.appendChild(inputCol);
+
+    let addBtnCol = document.createElement("div");
+    addBtnCol.className = "col-md-3";
+    const addFunction = function (e) {
+      e.preventDefault();
+      mainClass.insertData(this.form);
+    };
+    addBtnCol.appendChild(this.createButton("Save", addFunction));
+    row.appendChild(addBtnCol);
+
+    let refreshBtnCol = document.createElement("div");
+    refreshBtnCol.className = "col-md-3";
+    const refreshFunction = function (e) {
+      e.preventDefault();
+      this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+    };
+    refreshBtnCol.appendChild(this.createButton("Remove", refreshFunction));
+    row.appendChild(refreshBtnCol);
+    return row;
+  }
+
+  //create elements
+  createEle(Elem, type, text) {
+    const input = document.createElement(Elem);
+    if (Elem != "label") {
+      input.className = "form-control";
     }
 
-    //create Table
-    createTable()
-    {
-        let table = document.createElement('table');
-            table.className = "table table-responsive-sm";
-            table.id= this.formId;
-           
-        return table;            
-    }
+    input.type = type;
+    input.value = "";
+    input.textContent = text;
+    return input;
+  }
 
-    //create thead
-    createThead()
-    {
-            let thead= document.createElement('thead');
-            let row = document.createElement('tr');
-                row.name ="trRow";
-                row.className = "form-row";
-                
-                let labelTh = document.createElement('th');
-                let label = this.createEle('input','text');
-                    label.className="dyn-label"
-                    label.placeholder = "Enter Label / Id";
-                labelTh.appendChild(label);    
-                row.appendChild(labelTh);
-                
-                let dropDownTh = document.createElement('th');
-                let dropDown = document.createElement('select');
-                    dropDown.className="input-id";
-                    dropDownOptions.forEach((opt, index) =>{
-                        let optNode = document.createElement("option");
-                            optNode.value = opt.value;
-                            optNode.innerHTML = opt.innerHTML;
-                        return dropDown.appendChild(optNode);
-                    });
-                dropDownTh.appendChild(dropDown);
-                row.appendChild(dropDownTh);
+  //create buttons
+  createButton(label, btnFunction) {
+    const btn = document.createElement("button");
+    btn.textContent = label;
+    btn.onclick = btnFunction;
+    btn.className = "btn btn-primary";
+    return btn;
+  }
+}
 
-                let addBtnTd = document.createElement('td');   
-                let addFunction= function()  { console.log(row.childNodes ); }   
-                addBtnTd.appendChild(this.createButton("Add",addFunction)); 
-                row.appendChild(addBtnTd);
-            
-                let refreshBtnTd = document.createElement('td'); 
-                let refreshFunction= function() {  console.log(this);}   
-                refreshBtnTd.appendChild(this.createButton("Refresh",refreshFunction)); 
-                row.appendChild(refreshBtnTd);
-
-            thead.appendChild(row);      
-            return thead;       
-    }
-    // create tbody
-    createTbody()
-    {
-        let tbody = document.createElement('tbody');
-        return tbody;            
-    }
-
-    //creating Tr
-    createTr(label,type)
-    {
-        let row = document.createElement('tr'); 
-            
-            let labelTd = document.createElement('td');
-                labelTd.appendChild(this.createEle('label','',label));
-            row.appendChild(labelTd);
-
-            let inputTd = document.createElement('td');    
-                inputTd.appendChild(this.createEle('input',type));
-            row.appendChild(inputTd);
-
-            let saveBtnTd = document.createElement('td');   
-                let saveFunction= function() { console.log(this.label);}    
-                saveBtnTd.appendChild(this.createButton("Save",saveFunction)); 
-            row.appendChild(saveBtnTd);
-            
-            let removeBtnTd = document.createElement('td'); 
-                let removeFunction= function() { console.log(this.label);}   
-            removeBtnTd.appendChild(this.createButton("Remove",removeFunction)); 
-            row.appendChild(removeBtnTd);
-
-        return row; 
-    }
-
-    //create elements
-    createEle(Elem,type,text)
-    {
-        let input = document.createElement(Elem);
-            input.type = type;
-            input.textContent = text;
-        return input;
-    }
-
-    //create buttons
-    createButton(label,btnFunction)
-    {
-        let btn = document.createElement('button');
-            btn.textContent =label;
-            btn.onclick= btnFunction; 
-        return btn;
-    }
+function getValues(form) {
+  setValues(form);
 }
